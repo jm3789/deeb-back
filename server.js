1b78b4431f5b3d44f13c4408361332c69c829a11
jm3789 <jm37890@gmail.com>
2024-03-11 22:50:33 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(8000, () => {
+	// 8000번 포트로 서버 실행
+	console.log("listening on 8000");
+});
+
+app.use(express.static(path.join(__dirname, "dib-front/build")));
+
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "../dib-front/build/index.html"));
+});
