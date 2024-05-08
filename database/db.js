573befe3aacb0c1cbce3ec95100262d9e12cebb4
jm3789 <jm37890@gmail.com>
2024-05-08 11:29:40 +0900

    feat: .env 추가 및 db 연결

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const dbConncection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'deeb_book_schema'
+});
+
+dbConncection.connect((e) => {
+    if (e) console.log(e);
+    else console.log('db 연결 성공');
+})
+
+module.exports = conn;
--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const express = require("express");
+const db = require("database/db");
+
 const app = express();
 const path = require("path");
 
-app.listen(8000, () => {
-	// 8000번 포트로 서버 실행
-	console.log("listening on 8000");
-});
-
 app.use(express.static(path.join(__dirname, "../deeb-front/build")));
-
-app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, "../deeb-front/build/index.html"));
-});
 
 app.use(
 	"/v1",
@@ -22,3 +15,22 @@ app.use(
 		changeOrigin: true,
 	})
 );
+
+app.get('/', (req, res) => {
+	// db에서 쿼리 실행
+    db.query('SELECT * FROM table_name', function(e, results, fields) {
+        if (e) throw err;
+		// 결과를 json 형식으로 클라이언트에 반환
+        res.send(results);
+    });
+});
+
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../deeb-front/build/index.html"));
+});
+
+app.listen(8000, () => {
+	// 8000번 포트로 서버 실행
+	console.log("listening on 8000");
+});
