e78d9257e157f71716ac2558b60507bddb1d37f2
jm3789 <jm37890@gmail.com>
2024-05-08 11:31:50 +0900

    chore: 코드 포맷팅

--- database/db.js
+++ database/db.js
@@ -1,16 +1,16 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const dbConncection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'deeb_book_schema'
+  host: "localhost",
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "deeb_book_schema",
 });
 
 dbConncection.connect((e) => {
-    if (e) console.log(e);
-    else console.log('db 연결 성공');
-})
+  if (e) console.log(e);
+  else console.log("db 연결 성공");
+});
 
 module.exports = conn;
--- server.js
+++ server.js
@@ -9,28 +9,27 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "../deeb-front/build")));
 
 app.use(
-	"/v1",
-	createProxyMiddleware({
-		target: "https://openapi.naver.com",
-		changeOrigin: true,
-	})
+  "/v1",
+  createProxyMiddleware({
+    target: "https://openapi.naver.com",
+    changeOrigin: true,
+  })
 );
 
-app.get('/', (req, res) => {
-	// db에서 쿼리 실행
-    db.query('SELECT * FROM table_name', function(e, results, fields) {
-        if (e) throw err;
-		// 결과를 json 형식으로 클라이언트에 반환
-        res.send(results);
-    });
+app.get("/", (req, res) => {
+  // db에서 쿼리 실행
+  db.query("SELECT * FROM table_name", function (e, results, fields) {
+    if (e) throw err;
+    // 결과를 json 형식으로 클라이언트에 반환
+    res.send(results);
+  });
 });
 
-
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "../deeb-front/build/index.html"));
+  res.sendFile(path.join(__dirname, "../deeb-front/build/index.html"));
 });
 
 app.listen(8000, () => {
-	// 8000번 포트로 서버 실행
-	console.log("listening on 8000");
+  // 8000번 포트로 서버 실행
+  console.log("listening on 8000");
 });
